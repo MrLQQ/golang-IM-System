49e52082090cf29b29ef6e21b15d48a15bd9dc42
louqiangqiang <louqiangqiang12478@apexsoft.com.cn>
2024-11-19 14:29:50 +0800

    用户业务封装：用户上线；用户下线；用户消息处理；

--- user.go
+++ user.go
@@ -7,9 +7,11 @@ type User struct {
 	Addr string
 	C    chan string
 	conn net.Conn
+
+	server *Server
 }
 
-func NewUser(conn net.Conn) *User {
+func NewUser(conn net.Conn, server *Server) *User {
 	userAddr := conn.RemoteAddr().String()
 
 	// 创建一个用户的API
@@ -18,11 +20,41 @@ func NewUser(conn net.Conn) *User {
 		Addr: userAddr,
 		C:    make(chan string),
 		conn: conn,
+
+		server: server,
 	}
 	// 启动监听当前user channel消息的goroutine
 	go user.ListenMessage()
 
 	return user
+}
+
+// 用户上线的业务
+func (this *User) OnLine() {
+
+	// 用户上线，将用户上入道onlineMap中
+	this.server.mapLock.Lock()
+	this.server.OnlineMap[this.Name] = this
+	this.server.mapLock.Unlock()
+
+	// 广播当前用户上线消息
+	this.server.BroadCast(this, "已上线")
+}
+
+// 用户下线的业务
+func (this *User) OffLine() {
+	// 用户下线，将用户从onlineMap中删除
+	this.server.mapLock.Lock()
+	delete(this.server.OnlineMap, this.Name)
+	this.server.mapLock.Unlock()
+
+	// 广播当前用户下线消息
+	this.server.BroadCast(this, "下线")
+}
+
+// 用户处理消息的业务
+func (this *User) DoMessage(msg string) {
+	this.server.BroadCast(this, msg)
 }
 
 // 监听当前user channel的方法，一旦有消息，就发送给对端客户端
