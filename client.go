15ae8675fa26c07022500907ffcccf09741499bf
louqiangqiang <louqiangqiang12478@apexsoft.com.cn>
2024-11-20 10:26:17 +0800

    实现私聊模式

--- client.go
+++ client.go
@@ -67,6 +67,46 @@ func (client *Client) menu() bool {
 	return true
 }
 
+// 查询在线用户
+func (client *Client) SelectUsers() {
+	sendMsg := "who\n"
+	_, err := client.conn.Write([]byte(sendMsg))
+	if err != nil {
+		fmt.Println("conn.Write err:", err)
+		return
+	}
+}
+func (client *Client) PrivateChat() {
+	// 提示用户选择聊天对象
+	client.SelectUsers()
+
+	var remoteName string
+	var chatMsg string
+	fmt.Println(">>>>>请输入聊天对象(用户名),exit退出:")
+	fmt.Scanln(&remoteName)
+	for remoteName != "exit" {
+		fmt.Println(">>>>>请输入聊天内容,exit退出:")
+		fmt.Scanln(&chatMsg)
+		for chatMsg != "exit" {
+			// 消息不为空发送
+			if len(chatMsg) != 0 {
+				sendMsg := "to|" + remoteName + "|" + chatMsg + "\n\n"
+				_, err := client.conn.Write([]byte(sendMsg))
+				if err != nil {
+					fmt.Println("conn.Write err:", err)
+					break
+				}
+			}
+			chatMsg = ""
+			fmt.Println(">>>>>请输入消息内容,exit退出:")
+			fmt.Scanln(&chatMsg)
+		}
+		client.SelectUsers()
+		fmt.Println(">>>>>请输入聊天对象(用户名),exit退出:")
+		fmt.Scanln(&remoteName)
+	}
+}
+
 func (client *Client) PublicChat() {
 	// 提示用户输入消息
 	var chatMsg string
@@ -115,7 +155,7 @@ func (client *Client) Run() {
 			break
 		case 2:
 			// 私聊模式
-			fmt.Println("私聊模式选择...")
+			client.PrivateChat()
 			break
 		case 3:
 			// 更新用户名
